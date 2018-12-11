dded41fb53f930fef11973700f6e1f1142c6cc85
McKeown <jam771@miami.edu>
2018-12-11 00:21:58 -0500

    Added some more helper functions, joined some objects together, and added simple colors!

---
+++ affineLinear/config.py
@@ -0,0 +1,8 @@
+
+# NECESSARY CONFIGURATION!!! ###################################################
+# This is the file where all configuration other than projectPath should go 
+
+# blenderVersion = "2.7"
+blenderVersion = "2.8"
+
+################################################################################
